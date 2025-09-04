8fa202a389c2a5658930671dbe8a3a414bb544a1
Michael Jones <michael.jones90@me.com>
2025-09-04 17:02:41 +0100

    Initial Commit

---
+++ ComplexityExplorer/ComplexityExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexityExplorerApp.swift
+//  ComplexityExplorer
+//
+//  Created by Michael Jones on 04/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ComplexityExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
